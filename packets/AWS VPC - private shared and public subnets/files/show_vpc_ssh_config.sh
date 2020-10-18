8de6e2f45c4bcd29aee707b05473b876bb2612d6
Jeroen Manders <jeroen@manders.be>
2020-10-18 11:30:34 +0000

    Move ssh generation

--- packets/AWS VPC - private shared and public subnets/files/show_vpc_ssh_config.sh
+++ packets/AWS VPC - private shared and public subnets/files/show_vpc_ssh_config.sh
@@ -1,2 +1,2 @@
 export AWS_DEFAULT_REGION="$aws_vpc_region";
-$MODULES_ROOT/github.com/infraxys-aws/aws-commons/master/utils/generate_vpc_ssh_config.py "$vpc_name"
+$MODULES_ROOT/github.com/infraxys-aws/aws-commons/master/python/infraxys_aws/utils/generate_vpc_ssh_config.py "$vpc_name"
